2b79a0adf267527f18cca342a86962946a526009
Saiful-hero <saif061096@gmail.com>
2023-12-07 20:11:00 +0600

    addded some class

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Player from './components/Player/Player'
 import Team from './components/Team/Team'
 import Expenditures from './components/Expenditures/Expenditures'
 import './App.css'
-import {Alert} from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -15,7 +15,7 @@ function App() {
 
   useEffect(() => {
     setPlayers(data);
-  },[])
+  }, [])
 
   const handlePlusButton = (player) => {
     const newTeam = [...team, player];
@@ -24,35 +24,35 @@ function App() {
 
   return (
     <>
-    
-        <div>
+
+      <div>
         <Container>
           <Alert variant='danger'>This is success</Alert>
-          <h1>Unsold Players</h1>
+          <h1 className='bg-primary'>Unsold Players</h1>
           <div className='grid-container'>
             {
-                players.map(player => <Player player={player} handlePlusButton={handlePlusButton} key={player.id}></Player>)
+              players.map(player => <Player player={player} handlePlusButton={handlePlusButton} key={player.id}></Player>)
             }
-            
+
           </div>
-          
-            <Row style={{marginTop:'20px'}}>
-            
-              <Col><Expenditures cart={team}></Expenditures></Col>
-              
-              <Col>
-                <div className='grid-container'>
+
+          <Row style={{ marginTop: '20px' }}>
+
+            <Col className='bg-info'><Expenditures cart={team}></Expenditures></Col>
+
+            <Col>
+              <div className='grid-container'>
                 {
                   team.map(t => <Team team={t} key={t.id}></Team>)
                 }
               </div>
-              </Col>
-            </Row>
-         
-            </Container>
-        </div>
-       
-          
+            </Col>
+          </Row>
+
+        </Container>
+      </div>
+
+
     </>
   )
 }
--- src/components/Expenditures/Expenditures.jsx
+++ src/components/Expenditures/Expenditures.jsx
@@ -3,14 +3,14 @@ import './Expenditures.css';
 
 const Expenditures = (props) => {
     const cart = props.cart;
-    const totalBudget = cart.reduce((sum,user)=>sum+user.base_price,0)
-    return(
+    const totalBudget = cart.reduce((sum, user) => sum + user.base_price, 0)
+    return (
         <>
-        <div className="">
-            <h1>Expenditures</h1>
-            <h4>Player Sold: {cart.length}</h4>
-            <h4>Total Spend: {totalBudget}</h4>
-        </div>
+            <div className="">
+                <h1>Expenditures</h1>
+                <h4>Player Sold: {cart.length}</h4>
+                <h4>Total Spend: {totalBudget}</h4>
+            </div>
         </>
     );
 };
